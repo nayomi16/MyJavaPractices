4d4357b1fe962b98128788829a0849c96f3fca6f
nayomi16 <nayomirashmikaaa@gmail.com>
2020-12-27 01:30:16 +0530

    Added Lamda functions

---
+++ src/IQ/LamdaFun.java
@@ -0,0 +1,17 @@
+package IQ;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class LamdaFun {
+    public static void main(String[] args) {
+
+        Function<Integer,Integer> f=i ->i*i;
+        System.out.println("squre of 2= "+f.apply(2));
+        System.out.println("squre of 5= "+f.apply(5));
+
+        Predicate<Integer> predicate=i ->i%2==0;
+        System.out.println(predicate.test(6));
+        System.out.println(predicate.test(7));
+    }
+}
