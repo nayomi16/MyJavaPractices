37d48c997b62cddf5e4fabb89b254f2e023c9c31
nayomi16 <nayomirashmikaaa@gmail.com>
2020-12-29 02:05:41 +0530

    Functional interface & lambda expression

--- src/IQ/LamdaFun.java
+++ src/IQ/LamdaFun.java
@@ -6,12 +6,22 @@ import java.util.function.Predicate;
 public class LamdaFun {
     public static void main(String[] args) {
 
-        Function<Integer,Integer> f=i ->i*i;
-        System.out.println("squre of 2= "+f.apply(2));
-        System.out.println("squre of 5= "+f.apply(5));
+//        Function<Integer,Integer> f=i ->i*i;
+//        System.out.println("squre of 2= "+f.apply(2));
+//        System.out.println("squre of 5= "+f.apply(5));
+//
+//        Predicate<Integer> predicate=i ->i%2==0;
+//        System.out.println(predicate.test(6));
+//        System.out.println(predicate.test(7));
+        A a=(int k)-> System.out.println("Lambda expression "+k);
+        a.m1(5);
 
-        Predicate<Integer> predicate=i ->i%2==0;
-        System.out.println(predicate.test(6));
-        System.out.println(predicate.test(7));
     }
+
+}
+@FunctionalInterface
+interface A{
+    public void m1(int a);
+
+
 }
